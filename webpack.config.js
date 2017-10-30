1550ba97acd5cf0040e4d630b17259a02b5ef4ac
Thibaut Van Spaandonck <thibaut@urge2code.com>
2017-10-30 15:21:19 +0100

    Configured source maps

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,11 @@ const config = {
   module: {
     loaders: [
       {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader"
+      },
+      {
         test: /\.tsx?$/,
         loader: 'ts-loader',
         exclude: /node_modules/
